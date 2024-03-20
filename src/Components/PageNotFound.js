aaf40ef8b932adb2e6381b989b9c026108214139
amit97s <amit1397singh@gmail.com>
2024-03-20 18:31:48 +0530

    hey himanshu

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -1,25 +1,7 @@
 import React from "react";
-import Slider from "react-slick";
 
 const PageNotFound = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  };
-  return (
-    <div>
-      <Slider {...settings}>
-        <div>Slide 1</div>
-        <div>Slide 2</div>
-        <div>Slide 3</div>
-      </Slider>
-    </div>
-  );
+  return <div>PageNotFound</div>;
 };
 
 export default PageNotFound;
