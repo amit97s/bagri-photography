fe9b71d611cdbc4cc0cfd6fa7abc529248fbc859
amit97s <amit1397singh@gmail.com>
2024-03-21 15:51:13 +0530

    some changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,60 +18,37 @@ import { Link, NavLink, useNavigate } from "react-router-dom";
 const Footer = () => {
   const navigate = useNavigate();
   return (
-    <div className="mt-32 sm:mt-20">
-      <div className="footer ">
-        <div className="sections md:12">
-          <img src={bimg36} alt="img1" />
+    <section className="bg-blue-600">
+      <section className=" flex items-center justify-between px-5 py-14 mt-5">
+        <div className="flex flex-col md:flex-row gap-4 items-center font-medium no-underline text-white">
+          <div> Home</div>
+          <div> About</div>
+          <div> Investment</div>
+          <div> Portfolio</div>
+          <div> Client</div>
+          <div> Contact</div>
         </div>
-        <div className="sections">
-          <img src={bimg37} alt="img1" />
-        </div>
-        <div className="sections">
-          <img src={bimg38} alt="img1" />
-        </div>
-        <div className="sections">
-          <img src={bimg39} alt="img1" />
-        </div>
-        {/* <div className="sections">
-          <img src={img5} alt="img1" />
-        </div>
-        <div className="sections">
-          <img src={img6} alt="img1" />
-        </div> */}
-      </div>
-
-      <section className="bg-blue-600">
-        <section className=" flex items-center justify-between px-5 py-14 mt-5">
-          <div className="flex flex-col md:flex-row gap-4 items-center font-medium no-underline text-white">
-            <div> Home</div>
-            <div> About</div>
-            <div> Investment</div>
-            <div> Portfolio</div>
-            <div> Client</div>
-            <div> Contact</div>
-          </div>
-          <div className="flex gap-3 items-center text-white">
-            <FaFacebook size={25} className="hover:text-blue-700" />
-            <FaInstagram
-              size={25}
-              className="hover:text-pink-500"
-              onClick={() =>
-                navigate(
-                  window.open(
-                    "https://www.instagram.com/bajrang_bagri_photography/"
-                  )
+        <div className="flex gap-3 items-center text-white">
+          <FaFacebook size={25} className="hover:text-blue-700" />
+          <FaInstagram
+            size={25}
+            className="hover:text-pink-500"
+            onClick={() =>
+              navigate(
+                window.open(
+                  "https://www.instagram.com/bajrang_bagri_photography/"
                 )
-              }
-            />
+              )
+            }
+          />
 
-            <FaXTwitter size={25} className="hover:text-black" />
-          </div>
-        </section>
-        <h1 className="text-center text-white text-base pb-5">
-          All content is copyright &copy; 2024 TH amit manral
-        </h1>
+          <FaXTwitter size={25} className="hover:text-black" />
+        </div>
       </section>
-    </div>
+      <h1 className="text-center text-white text-base pb-5">
+        All content is copyright &copy; 2024 TH amit manral
+      </h1>
+    </section>
   );
 };
 
