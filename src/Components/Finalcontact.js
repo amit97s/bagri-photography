54d9028f8fc48179928e6a05781d09b18276cf10
amit97s <amit1397singh@gmail.com>
2024-03-22 10:45:00 +0530

    form valid

--- src/Components/Finalcontact.js
+++ src/Components/Finalcontact.js
@@ -1,27 +1,176 @@
-import React from "react";
-import style from "./Finalcontact.css";
-import finimg from "../assets/VP010917-3-1024x683.jpg";
+import React, { useState } from "react";
+import img from "../subassets/bagris images/_AJU1479.jpg";
 const Finalcontact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [budget, setBudget] = useState("");
+  const [city, setCity] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = () => {
+    if (!name || !email || !phone) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
+  console.log(error);
   return (
-    <div className="Finalcontact">
-      <div className="Finalcontact-bgimg">
-        <div className=" flex justify-center ">
-          <img
-            src={finimg}
-            className="h-full mx-10 sm:mx-0 sm:h-72 md:h-[30rem] rounded-lg  duration-150"
-          />
-        </div>
-        <div className="para">
-          <p>
-            Want to make us a part of your celebration? Fill out the form below
-            with your details and we will get back to you as soon as we can. We
-            take up very limited weddings per year so please provide as many
-            details as you can like – dates, venues, ceremonies, wedding theme,
-            and a few lines about your story.
-          </p>
+    <>
+      <div className="relative ">
+        <img src={img} alt="img" className=" w-screen h-[34rem] object-cover" />
+
+        <div className="bg-white shadow-lg rounded-md w-[28rem] p-4 absolute  right-10 top-12 ">
+          <form
+            className="flex flex-col gap-2.5"
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <div className="flex items-center gap-2">
+              <div className="w-1/2">
+                <label
+                  htmlFor="UserEmail"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {" "}
+                  Name*
+                </label>
+
+                <input
+                  type="text"
+                  placeholder={error ? "Name is required" : "john doe"}
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  className={`mt-1 w-full rounded-md   sm:text-sm outline-none border  py-2.5 px-2 ${
+                    error ? "placeholder-red-600" : ""
+                  }`}
+                />
+              </div>
+
+              <div className="w-1/2">
+                <label
+                  htmlFor="UserEmail"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {" "}
+                  Email*{" "}
+                </label>
+
+                <input
+                  type="email"
+                  placeholder={
+                    error ? "email is required" : "amitmanral@gmail.com"
+                  }
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className={`mt-1 w-full rounded-md   sm:text-sm outline-none border border-gray-900 py-2.5 px-2 ${
+                    error ? "placeholder-red-600" : ""
+                  }`}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="UserEmail"
+                className="block text-sm font-medium text-gray-700"
+              >
+                {" "}
+                Phone*{" "}
+              </label>
+
+              <input
+                type="text"
+                placeholder={
+                  error ? "Phone number is required" : "+91 1234567890"
+                }
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+                className={`mt-1 w-full rounded-md   sm:text-sm outline-none border border-gray-900 py-2.5 px-2 ${
+                  error ? "placeholder-red-600 " : ""
+                }`}
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <div className="w-1/2">
+                <label
+                  htmlFor="HeadlineAct"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  {" "}
+                  Budget*{" "}
+                </label>
+
+                <select
+                  name="HeadlineAct"
+                  // value={budget}
+                  className="mt-1 w-full rounded-md   sm:text-sm outline-none border border-gray-900 py-2.5 px-2"
+                >
+                  <option value="">select budget</option>
+                  <option value="JM">100$</option>
+                  <option value="SRV">200$</option>
+                  <option value="JH">300$</option>
+                  <option value="BBK">400$</option>
+                  <option value="AK">500$</option>
+                </select>
+              </div>
+
+              <div className="w-1/2">
+                <label
+                  htmlFor="HeadlineAct"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  {" "}
+                  City*{" "}
+                </label>
+
+                <select
+                  name="HeadlineAct"
+                  // value={city}
+                  className="mt-1 w-full rounded-md   sm:text-sm outline-none border border-gray-900 py-2.5 px-2"
+                >
+                  <option value="">Please select</option>
+                  <option value="JM">Noida</option>
+                  <option value="SRV">Faridabad</option>
+                  <option value="JH">New Delhi</option>
+                  <option value="BBK">Ghaziabad</option>
+                  <option value="AK">Meerut</option>
+                  <option value="BG">Old Delhi</option>
+                  <option value="EC">other</option>
+                </select>
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="OrderNotes"
+                className="block text-sm font-medium text-gray-700"
+              >
+                {" "}
+                Message{" "}
+              </label>
+
+              <textarea
+                className="mt-1 w-full rounded-md   sm:text-sm outline-none border border-gray-900 py-2.5 px-2"
+                rows="4"
+                placeholder="Enter your message......."
+              ></textarea>
+            </div>
+
+            <div>
+              <button
+                onClick={handleSubmit}
+                className="inline-block w-full rounded border bg-red-400 px-12 py-3 text-sm font-medium text-white hover:bg-red-500 hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
